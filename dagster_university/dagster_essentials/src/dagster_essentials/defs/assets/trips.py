d91ead5246276119e20e5a55725e4470b82f3715
John Poulis <ioanniswd@gmail.com>
2025-11-30 22:22:22 +0200

    Use DuckDBResource in taxi_zones asset

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -64,7 +64,7 @@ def taxi_trips(database: DuckDBResource) -> None:
 @dg.asset(
     deps=['taxi_zones_file']
 )
-def taxi_zones() -> None:
+def taxi_zones(database: DuckDBResource) -> None:
     """
     The raw taxi zones dataset, Loaded into a DuckDB database
     """
@@ -80,12 +80,5 @@ def taxi_zones() -> None:
       );
     """
 
-    conn = backoff(
-        fn=duckdb.connect,
-        retry_on=(RuntimeError, duckdb.IOException),
-        kwargs={
-            "database": os.getenv("DUCKDB_DATABASE"),
-        },
-        max_retries=10,
-    )
-    conn.execute(query)
+    with database.get_connection() as conn:
+        conn.execute(query)
